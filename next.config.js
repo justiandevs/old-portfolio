74fa56857270947df1f036f0fce67cf1b8a753a2
justiandevs <justiandev@gmail.com>
2023-02-25 22:16:41 +0100

    Security Headers

--- next.config.js
+++ next.config.js
@@ -1,8 +1,56 @@
 /** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src 'self' 'typekit.com';
+  style-src 'self' 'typekit.com';
+  font-src 'self' 'typekit.com';  
+`
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
