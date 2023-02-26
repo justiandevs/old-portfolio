ed795e9e88b96bfd33ee5a95b8d0d9a7e7e4e42a
justiandevs <justiandev@gmail.com>
2023-02-26 18:01:17 +0100

    Work page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,34 @@ import {Hero} from "@/components/Hero";
 import {FaArrowRight, FaGit, FaGithub, FaKeyboard, FaPen, FaRocket} from "react-icons/fa";
 import Link from "next/link";
 import {Metadata} from "next";
+import fs from "fs";
+import matter from "gray-matter";
+
+export const dynamic = "error";
 
 export const metadata: Metadata = {
   title: 'Full stack freelance developer from the Netherlands - Justian.dev',
   description: 'I am Justian Spijkerbosch. A full stack developer from Apeldoorn, Netherlands. I have been a hobby programmer for a number of years and since start 2023 a freelance full stack developer',
 }
 
-export default function Home() {
+async function getLastThreeProjects() {
+  const projects = fs.readdirSync('./projects');
+
+  return projects.map((slug: string) => {
+    const source = fs.readFileSync('./projects/' + slug, 'utf-8');
+
+    const { data } = matter(source);
+    const time = fs.statSync('./projects/' + slug).mtime.getTime();
+    const date = fs.statSync('./projects/' + slug).mtime.toLocaleDateString('nl-NL');
+    const type = data.type;
+
+    return { data, time, date, type };
+  }).sort((a, b) => b.time - a.time).slice(0, 3);
+}
+
+export default async function Home() {
+  const projects = await getLastThreeProjects();
+
   return (
     <>
       <Hero title="Justian Spijkerbosch 😌" description="Freelance full stack developer" />
@@ -36,7 +57,17 @@ export default function Home() {
         <hr className="my-8" />
         <h2>My recent work</h2>
         {/* TODO: Implement recent work */}
-        <p>Todo: Implement recent work</p>
+        <div className="mt-8 grid grid-cols-1 gap-6">
+          {projects.map((project) => {
+            return (
+              <Link href={`/work/${project.data.slug}`} key={project.data.slug} className="post border border-stone-200 rounded-xl px-8 py-4 flex flex-col gap-2">
+                <h3>{project.data.title}</h3>
+                <p>{project.data.description}</p>
+                <p className="font-bold underline text-green-700">View more</p>
+              </Link>
+            )
+          })}
+        </div>
       </section>
     </>
   )
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,12 +1,86 @@
 import {Metadata} from "next";
+import fs from "fs";
+import matter from "gray-matter";
+import {Hero} from "@/components/Hero";
+import Link from "next/link";
+
+export const dynamic = 'error';
 
 export const metadata: Metadata = {
   title: 'My client work and private projects - Justian.dev',
   description: 'A short overview from the projects I worked on, differs between client work and personal opensource projects',
 }
 
-export default function Work() {
+async function getAllProjects() {
+  const projects = fs.readdirSync('./projects');
+
+  const sortedProjects = projects.map((slug: string) => {
+    const source = fs.readFileSync('./projects/' + slug, 'utf-8');
+
+    const { data } = matter(source);
+    const time = fs.statSync('./projects/' + slug).mtime.getTime();
+    const date = fs.statSync('./projects/' + slug).mtime.toLocaleDateString('nl-NL');
+    const type = data.type;
+
+    return { data, time, date, type };
+  }).sort((a, b) => b.time - a.time)
+
+  return {
+    featured: sortedProjects.filter((project) => project.type === "featured"),
+    forOthers: sortedProjects.filter((project) => project.type === "for-others"),
+    opensource: sortedProjects.filter((project) => project.type === "opensource")
+  }
+}
+
+export default async function Work() {
+  const { featured, forOthers, opensource } = await getAllProjects();
+
   return (
-    <h1>My work 😌</h1>
+    <>
+      <Hero title="My work 🎨" description="I like to build on awesome projects" />
+      <section className="content">
+        <h2>Featured</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8 mt-6">
+          {featured.map((project) => {
+            return (
+              <Link href={`/work/${project.data.slug}`} key={project.data.slug} className="post border border-stone-200 rounded-xl px-8 py-4 flex flex-col gap-2">
+                <h3>{project.data.title}</h3>
+                <p>{project.data.description}</p>
+                <p className="font-bold underline text-green-700">View more</p>
+              </Link>
+            )
+          })}
+          <Link href="/contact" className="post border border-stone-200 rounded-xl px-8 py-4 flex flex-col gap-2 opacity-70">
+            <h3>Your project here?</h3>
+            <p>Need a full-stack developer? I stay open for working on new cool projects.</p>
+            <p className="text-green-700 font-bold underline">Hire me</p>
+          </Link>
+        </div>
+        <h2>Opensource</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8 mt-6">
+          {opensource.map((project) => {
+            return (
+              <Link href={`/work/${project.data.slug}`}  key={project.data.slug} className="post border border-stone-200 rounded-xl px-8 py-4 flex flex-col gap-2">
+                <h3>{project.data.title}</h3>
+                <p>{project.data.description}</p>
+                <p className="font-bold underline text-green-700">View more</p>
+              </Link>
+            )
+          })}
+        </div>
+        <h2>For others</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8 mt-6">
+          {forOthers.map((project) => {
+            return (
+              <Link href={`/work/${project.data.slug}`}  key={project.data.slug} className="post border border-stone-200 rounded-xl px-8 py-4 flex flex-col gap-2">
+                <h3>{project.data.title}</h3>
+                <p>{project.data.description}</p>
+                <p className="font-bold underline text-green-700">View more</p>
+              </Link>
+            )
+          })}
+        </div>
+      </section>
+    </>
   )
 }
