24e63ad4961e79f113715b635c331e4ddf74e62c
justiandevs <justiandev@gmail.com>
2023-02-27 19:27:50 +0100

    Vercel fix

--- src/app/minecraft/page.tsx
+++ src/app/minecraft/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import minecraft from '../../../public/images/2023-02-27_19.09.19.png';
 
 export const metadata: Metadata = {
-  title: "Justian.dev's minecraft server - Justian.dev",
+  title: "Justian.dev minecraft server - Justian.dev",
   description: "My minecraft server is a server focused on anyone who wants to play fun survival minecraft, the server is on \"hard\" mode and is currently running on version 1.19.3."
 }
 
@@ -15,8 +15,8 @@ export default function Minecraft() {
       <Hero title="Vanilla minecraft server" description="Whitelisted minecraft 1.19 server for everyone who wants to join" />
       <section className="content">
         <Image className="rounded-xl mb-8" src={minecraft} alt="View on the spawn biome of my minecraft server" />
-        <h2>Justian.dev's minecraft server</h2>
-        <p className="mt-2">My minecraft server is a server focused on anyone who wants to play fun survival minecraft, the server is on "hard" mode and is currently running on version 1.19.3.</p>
+        <h2>Justian.dev minecraft server</h2>
+        <p className="mt-2">My minecraft server is a server focused on anyone who wants to play fun survival minecraft, the server is on hard mode and is currently running on version 1.19.3.</p>
         <p className="mt-6">So do you feel like playing vanilla minecraft occasionally for an evening with a friendly community around you? Then I would recommend contacting me through my <a href="https://discord.gg/53xGDZ5V" target="_blank">discord server</a> to get access to the whitelist.</p>
         <hr className="my-8" />
         <h2>Ip address</h2>
